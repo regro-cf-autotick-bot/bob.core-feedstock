cf03ad6aaed13ac9c8244bc4b6672522832de2d2
Amir Mohammadi <183.amir@gmail.com>
2016-05-22 00:47:00 +0200

    pin boost (#5)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,6 @@
 
 if [[ `uname` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
-	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
-else
-	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
 fi
+
+$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
